571b984162328c4e44b0ae0fae8e1f3ccc11c257
Arkild <koray.semenuik@gmail.com>
2023-09-06 11:25:03 -0600

    successfully looped display through lists

--- resume/app.js
+++ resume/app.js
@@ -2,17 +2,17 @@
 let job1 = false;
 let job2 = false;
 const exjob1 = document.querySelector(".expandjob1")
-const hidjob1 = document.querySelector(".hiddenjob1")
+const hidjob1 = document.querySelectorAll(".hiddenjob1")
 
 
 exjob1.addEventListener("click", () => {
     //this initially worked after 2 clicks when "none" and "list-item" were swapped. This lets it work in one click and I have ABSOLUTELY NO IDEA why. If it works, it works.
-    if (hidjob1.style.display === 'list-item') {
-        hidjob1.style.display = "none";
-        console.log("debug")
-    } else {
-        hidjob1.style.display = "list-item";
-        console.log("debug")
+    for (jobDesc of hidjob1) {
+        if (jobDesc.style.display === 'list-item') {
+            jobDesc.style.display = "none" ;
+        } else {
+            jobDesc.style.display = "list-item";
+        }
     }
     //Switch between classes that define the hand character on this line
     const handTog1 = document.getElementById("job1hand");
