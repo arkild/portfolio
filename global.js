6a69d5e4ca3a3b310ac9eb3974b216edd44708ba
Arkild <koray.semenuik@gmail.com>
2023-09-07 16:25:51 -0600

    added responsive layout change to move the navbar to global javascript code

--- global.js
+++ global.js
@@ -8,3 +8,26 @@ tomorrow.setDate(today.getDate() + 1) //Tomorrow's date
 //No matter what, this will correctly display tomorrow's date. I had to +1 the month because the month display begins at 0. If I wanted to make the month display as a word, I would need to create an array representing the months and access it that way.
 dateDisplay.innerText = `${tomorrow.getMonth() + 1}/${tomorrow.getDate()}`
 
+
+function changeLayout() {
+    if (window.innerWidth > 800) {
+        document.querySelector("div").classList.add("row");
+        document.querySelector(".navbar").classList.add("col-4");
+        document.querySelector("ul").classList.add("flex-column");
+        const contentBox = document.querySelector(".content");
+            contentBox.classList.add("col-8");
+            contentBox.classList.add("order-last");
+    } else {
+        document.querySelector("div").classList.remove("row");
+        document.querySelector(".navbar").classList.remove("col-4");
+        document.querySelector("ul").classList.remove("flex-column");
+        const contentBox = document.querySelector(".content");
+            contentBox.classList.remove("col-8");
+            contentBox.classList.remove("order-last");
+    }
+}
+changeLayout();
+window.addEventListener('resize', changeLayout)
+
+
+// Layout change rough draft. Will deal with later.
